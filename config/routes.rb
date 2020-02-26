edf4a6828d75fdd3f8184ec16f8cc1dcd52e37f4
saulpereira <saul_pereira@hotmail.com>
2020-02-26 08:36:08 -0600

    Added user and shout page

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,14 @@ Rails.application.routes.draw do
   end
 
   root to: "homes#show"
-  resources :shouts, only: [:create]
+  resources :shouts, only: [:create, :show]
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
-  resources :users, only: [:create] do
+  resources :users, only: [:create, :show] do
     resource :password,
       controller: "clearance/passwords",
-      only: [:edit, :update]
+      only: [:create, :edit, :update]
   end
 
   get "/sign_in" => "sessions#new", as: "sign_in"
